9e2ad691f1fa99d916326e3aa55c88172667b75e
lindsaygelle <lindsaygelle@yahoo-inc.com>
2016-09-12 09:12:32 +1000

    Edited animate to immediately call after initialised.

--- js/animationframe.js
+++ js/animationframe.js
@@ -1,9 +1,8 @@
 window.keyframe = {};
 
 keyframe.start = function (functions) {
-	this.functions = functions;
-	this.animate = (keyframe.functions.length) ? function (delta) { for (var i = 0, i < keyframe.functions.length; i++) {keyframe.functions[i](delta)} window.keyframe.id = window.requestAnimationFrame(keyfram.animate);} : function (delta) { keyframe.functions(delta) };
-	this.animate();
+	this.functions = functions || function (delta) { console.log(delta)};
+	this.animate = (keyframe.functions.length) ? function (delta) { for (var i = 0, i < keyframe.functions.length; i++) {keyframe.functions[i](delta)} window.keyframe.id = window.requestAnimationFrame(keyfram.animate);} : function (delta) { keyframe.functions(delta) }();
 };
 
 keyframe.end = function (callback) {
