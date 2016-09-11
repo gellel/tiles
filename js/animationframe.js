ce4c4325f2f6e198c374bb118d0a5eae45081fa5
lindsaygelle <lindsaygelle@yahoo-inc.com>
2016-09-12 09:11:22 +1000

    Simplified function call for animate to remove loop overhead check.

--- js/animationframe.js
+++ js/animationframe.js
@@ -2,19 +2,11 @@ window.keyframe = {};
 
 keyframe.start = function (functions) {
 	this.functions = functions;
+	this.animate = (keyframe.functions.length) ? function (delta) { for (var i = 0, i < keyframe.functions.length; i++) {keyframe.functions[i](delta)} window.keyframe.id = window.requestAnimationFrame(keyfram.animate);} : function (delta) { keyframe.functions(delta) };
 	this.animate();
 };
 
 keyframe.end = function (callback) {
 	window.cancelAnimationFrame(keyframe.id);
 	if (callback) callback();
-}
-
-keyframe.animate = function (delta) {
-	if (keyframe.functions) {
-		for (var i = 0; i < keyframe.functions.length; i++) {
-			keyframe.functions[i](delta)
-		}
-	}
-	window.keyframe.id = window.requestAnimationFrame(keyfram.animate);
 };
