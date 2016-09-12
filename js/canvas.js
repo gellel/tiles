2b048e2edc5531d3c1966b540be9d6c25cb14c6e
lindsaygelle <lindsaygelle@yahoo-inc.com>
2016-09-12 18:01:25 +1000

    Updated files with comments for readability.

--- js/canvas.js
+++ js/canvas.js
@@ -1,100 +1,142 @@
 class canvas {
 
 	save () {
+		/** store this classes instance of the canvas state **/
 		this.context.save();
 	}
 
 	restore () {
+		/** restore this classes instance of the canvas state **/
 		this.context.restore();
 	}
 
 	drawRegExp (type) {
+		/** define types of rectangle drawing methods with their expression and system definitions **/
 		var methods = [{ key: "fillRect", exp: new RegExp("^fill(Rect)?$", "gi") }, { key: "clearRect", exp: new RegExp("^clear(Rect)?$", "gi") }, { key: "strokeRect", exp: new RegExp("^stroke?Rect$", "gi" ) }];	
+		/** iterate over defined method data **/
 		for (var i = 0, len = methods.length; i < len; i++) {
+			/** return system definition of fill method **/
 			if (type.match(methods[i].exp)) return methods[i].key;
 		};
 		return undefined;
 	}
 
 	drawGeometry (type, positionX, positionY, width, height, style) {
+		/** apply drawing styles for rectangle **/
 		if (style) this.drawApplyStyle(style);
+		/** draw rectangle using system method **/
 		this.context[this.drawRegExp(type) || "fillRect"](positionX, positionY, width, height);
 	}
 
 	drawImage (image, sourceX, sourceY, souceWidth, sourceHeight, positionX, positionY, imageWidth, imageHeight) {
+		/** draw image to canvas from image source **/
 		this.context.drawImage(image, sourceX, sourceY, souceWidth, sourceHeight, positionX, positionY, imageWidth, imageHeight)
 	}
 
 	drawApplyStyle (style) {
+		/** iterate over keys in style object **/
 		for (var key in style) {
+			/** apply 'style' to canvas context if key matches system definition **/
 			if (this.context[key]) this.context[key] = style[key];
 		}
 	}
 
 	drawBezier (curveXStart, curveYStart, curveXEnd, curveYEnd, positionXStart, positionYStart, positionXEnd, positionYEnd, style) {
+		/** start line path **/
 		this.context.beginPath();
+		/** apply drawing styles for strokes **/
 		if (style) this.drawApplyStyle(style);
+		/** set line starting position **/
 		this.context.moveTo(positionXStart, positionYStart);
+		/** draw bezier **/
 		this.context.bezierCurveTo(curveXStart, curveYStart, curveXEnd, curveYEnd, positionXEnd, positionYEnd);
+		/** produce stroked line across bezier **/
 		this.context.stroke();
 	}
 
 	drawBeziers () {
+		/** capture arguments to function **/
 		var parameters = Array.prototype.slice.call(arguments);
+		/** confirm if item can be iterated over **/
 		if (parameters[0].length) {
+			/** iterate over assumed array **/
 			for (var i = 0, beziers = parameters[0], len = beziers.length; i < len; i++) {
+				/** cache bezier item data **/
 				var bezier = beziers[i];
+				/** pass data to single bezier function **/
 				this.drawBezier(bezier.curve.x.s, bezier.curve.y.s, bezier.curve.x.e, bezier.curve.y.e, bezier.position.x.s, bezier.position.y.s, bezier.position.x.e, bezier.position.y.e, bezier.style);
 			}
 		}
 		else {
+			/** pass data to single bezier function **/
 			this.drawBezier(parameters);
-		}
+		};
 	}
 
 	drawLine (positionXStart, positionYStart, positionXEnd, positionYEnd, style) {
+		/** start line path **/
 		this.context.beginPath();
+		/** apply drawing styles for strokes **/
 		if (style) this.drawApplyStyle(style);
+		/** set line starting position **/
 		this.context.moveTo(positionXStart, positionYStart);
+		/** draw line **/
 		this.context.lineTo(positionXEnd, positionYEnd);
+		/** produce stroked line across line **/
 		this.context.stroke();
 	}
 
 	drawLines () {
+		/** capture arguments to function **/
 		var parameters = Array.prototype.slice.call(arguments);
+		/** confirm if item can be iterated over **/
 		if (parameters[0].length) {
+			/** iterate over assumed array **/
 			for (var i = 0, lines = parameters[0], len = lines.length; i < len; i++) {
+				/** cache line item data **/
 				var line = lines[i];
+				/** pass data to single line function **/
 				this.drawLine(line.x.s, line.y.s, line.x.e, (line.y.e || line.y.s), line.style);
 			}
 		}
 		else {
+			/** pass data to single line function **/
 			this.drawLine(parameters);
 		}
 	}
 
 	fontApplyAttributes (font) {
+		/** create single line string as defined css standard for shorthand font **/
 		return (font.style || 'normal') + ' ' + (font.variant || 'normal') + ' ' + (font.weight || 'normal') + ' ' + (font.stretch || 'normal') + ' ' + (font.size || '14px') + '/' + (font.line || 'inherit') + ' ' + (font.family || 'sans-serif');
 	}
 
 	drawFillText (positionX, positionY, fontString, fontAttributes, fontStyle) {
+		/** set font for canvas based on string as argument or object formatted to string **/
 		this.context.font = (typeof fontAttributes === 'string') ? fontAttributes : this.fontApplyAttributes(fontAttributes);
+		/** apply drawing styles for font **/
 		if (fontStyle) this.drawApplyStyle(fontStyle);
+		/** draw filled text **/
 		this.context.fillText(fontString, positionX, positionY);
 	}
 
 	drawStrokeText (positionX, positionY, fontString, fontAttributes, fontStyle) {
+		/** set font for canvas based on string as argument or object formatted to string **/
 		this.context.font = (typeof fontAttributes === 'string') ? fontAttributes : this.fontApplyAttributes(fontAttributes);
+		/** apply drawing styles for font **/
 		if (fontStyle) this.drawApplyStyle(fontStyle);
+		/** draw stroked text **/
 		this.context.strokeText(fontString, positionX, positionY);
 	}
 
 	create (node, attributes) {
+		/** create html canvas element with attributes (requires prototypes.js) **/
 		return node.insertNode("canvas", attributes);
 	}
 
 	constructor (node, attributes) {
+		/** create self instance of html canvas element **/
 		this.node = this.create(node || document.body, attributes || { id: 'canvas', width: 600, height: 400, style: 'border: 1px solid #e1e1e1;' });
+		/** create self instance of canvas context 2d **/
 		this.context = this.node.getContext("2d");
 	}
 
--- js/tiles.js
+++ js/tiles.js
@@ -1,55 +1,79 @@
 class tiles {
 
 	randRange (minimum, maximum) {
+		/** generate randon number based on min and max values **/
 		return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 	}
 
 	createTileObject (row, col, totalRows, totalColumns) {
+		/** set the base coordinates for tile item for the array **/
 		var map_data = { x: (col * this.width), y: (row * this.height) };
-
+		/** set tile type to be 1 if it is along the top or bottom row **/
 		if (row === 0 || row === totalRows - 1) {
 			map_data.i = 1;
 		}
+		/** set tile type to be 2 if it is along the left column **/
 		else if (col === 0) {
 			map_data.i = 2;
 		}
+		/** set tile type to be 3 if it is along the right column **/
 		else if (col === totalColumns - 1) {
 			map_data.i = 3;
 		}
+		/** set tile type to be either 0,3 if it is any other position if the random occurance check returns 0 **/
 		else {
 			map_data.i = (!this.randRange(0, 2)) ?  this.randRange(0, 3) : 0;
 		}
+		/** return complete object **/
 		return map_data;
 	}
 
 	createMapArray () {
+		/** set base container to hold 2d array **/
 		var map_full = [];
+		/** iterate over defined rows for this size of grid **/
 		for (var i = 0; i < this.rows; i++) {
+			/** set a base container to hold the current rows data **/
 			var map_array_line = [];
+			/** iterate over the defined columns for the current row **/
 			for (var j = 0; j < this.cols; j++) {
+				/** push current tile data to row array **/
 				map_array_line.push(this.createTileObject(i, j, this.rows, this.cols));
-			}
+			};
+			/** push complete row to main 2d array **/
 			map_full.push(map_array_line);
-		}
+		};
+		/** return complete 2d array **/
 		return map_full;
 	}
 
 	createExampleMap () {
+		/** iterate over length of 2d array rows **/
 		for (var i = 0, rlen = this.maps.length; i < rlen; i++) {
+			/** cache current array row **/
 			var map_array_line = this.maps[i];
+			/** iterate over array row columns **/
 			for (var j = 0, clen = map_array_line.length; j < clen; j++) {
+				/** cache current row and column data **/
 				var data = this.maps[i][j];
+				/** draw a simple rectangle using the canvas class defined method and supplied data **/
 				this.canvas.drawGeometry("fillRect", data.x, data.y, this.width, this.height, { fillStyle: 1 === data.i ? "red" : 2 === data.i ? "blue" : 3 === data.i ? "green" : "transparent" });	
 			}
 		}
 	}
 
 	constructor (canvas, scale) {
+		/** create self instance of canvas class (requires canvas.js) **/
 		this.canvas = canvas; 
+		/** create self instance tile sprite size width **/
 		this.width = scale;
+		/** create self instance tile sprite size height **/
 		this.height = scale;
+		/** create self instance number of rows based on physical canvas element size width **/
 		this.rows = this.canvas.node.height / this.height;
+		/** create self instance number of rows based on physical canvas element size height **/
 		this.cols = this.canvas.node.width / this.width;
+		/** create self instance of canvas grid 2d map **/
 		this.maps = this.createMapArray();
 	}
 }
