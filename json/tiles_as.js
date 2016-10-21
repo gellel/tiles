9967c5cab2cbd26683498b9d9f97697fba2afdf6
gellel <lindsay.gelle@gmail.com>
2016-10-21 11:13:32 +1100

    Good pallete working.

--- json/tiles_as.js
+++ json/tiles_as.js
@@ -4,109 +4,80 @@ var geography = {
 		tiles: [
 
 			{
-				colour: {
-					hex: "#098bde" 
-				}
+				colour: { hex: "#2f96d8" }
 			},
 			{
-				colour: {
-					hex: "#1891de" 
-				}
+				colour: { hex: "#3999d7" }
 			},
 			{
-				colour: {
-					hex: "#2296df" 
-				}
+				colour: { hex: "#3d9bd8" }
 			},
 			{
-				colour: {
-					hex: "#2f9ee3" 
-				}
+				colour: { hex: "#429dd8" }
 			},
 			{
-				colour: {
-					//hex: "#56afdb" 
-					hex: "#3ea5e0"
-				}
+				colour: { hex: "#449dd6" }
+			},
+			/** padding for rise **/
+			{
+				colour: { hex: "#469fd8" }
 			},
 			{
-				colour: {
-					//hex: "#56afdb" 
-					hex: "#48a9de"
-				}
+				colour: { hex: "#449dd6" }
+			},
+			/** coast **/
+			{
+				colour: { hex: "#54aedc" }
 			},
 			{
-				colour: {
-					//hex: "#a5d1cc" 
-					hex: "#6ab7d8"
-					//hex: "#5eafd2"
-				}
+				colour: { hex: "#60b3d9" }
 			},
 			{
-				colour: {
-					//hex: "#a5d1cc" 
-					hex: "#6ab7d8"
-					//hex: "#5eafd2"
-				}
+				colour: { hex: "#88c4d2" }
+			},
+			/** wash **/
+			
+			{
+				colour: { hex: "#f1f0d7"}
+			},
+
+			/** sand **/
+			{
+				colour: { hex: "#ebeacd" }
 			},
 			{
-				colour: {
-					//hex: "#cde2c5"
-					hex: "#90c8d0"
-				}
+				colour: { hex: "#e6e5c7" }
+			},
+			
+			
+			/** grass **/
+			{
+				colour: { hex: "#9fc59b" }
 			},
 			{
-				colour: {
-					hex: "#f4f3bd"
-				}
+				colour: { hex: "#94bf8f" }
 			},
 			{
-				colour: {
-					hex: "#ebeab6"
-				}
+				colour: { hex: "#8bba83" }
 			},
 			{
-				colour: {
-					hex: "#e4e3ad"
-				}
+				colour: { hex: "#85b87d" }
 			},
 			{
-				colour: {
-					//hex: "#cccb92"
-					//hex: "#d9d899"
-					hex: "#e0dfa5"
-				}
+				colour: { hex: "#76ab6e" }
 			},
 			{
-				colour: {
-					//hex: "#b8b779"
-					hex: "#b5cf83"
-				}
+				colour: { hex: "#66a05c" }
+			},
+			/** plain **/
+			{
+				colour: { hex: "#69a35f" }
 			},
 			{
-				colour: {
-					//hex: "#9ca86e"
-					hex: "#7ab66a"
-				}
-			},
-			{
-				colour: {
-					//hex: "#7c9e60"
-					hex: "#6fab5f"
-				}
-			},
-			{
-				colour: {
-					//hex: "#54965e"
-					hex: "#659d56"
-				}
-			},
-			{
-				colour: {
-					//hex: "#3a8e48"
-					hex: "#5c984c"
-				}
+				colour: { hex: "#64a05a" }
 			}
+			
+
 			
 			
 			
